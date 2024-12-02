6255f25278ad50eee67b2bb534f926c12d69f626
WW-CSC <wyatt@le-cerveau.co.uk>
2024-12-02 12:52:50 -0600

    Created user authentication

--- Project_4/php/login.php
+++ Project_4/php/login.php
@@ -19,77 +19,16 @@
         Enter button
         Create account button - redirects to Create account page. 
 */
-
-    if(isset($_POST['createAccount']))
-    {
-        header('Location: ..\html\createAccount.html');
-    }
-    
-    session_start();
-
-    class UserAuthenticator 
-    {
-        var $username;
-        var $password; 
-
-        var $correctUser = "CSC390";
-        var $correctPass = "php";
-
-        function isLoggedIn() : Bool
-        {
-            if($_SESSION["login"] == false || !isset($_SESSION["login"]))
-            {
-                return false;
-            }
-            
-            return true;
-        }
-
-        function authenticate($username, $password) : Bool
-        {
-            $this -> username = $username;
-            $this -> password = $password;
-
-            if($this->password == $this->correctPass && $this->username == $this->correctUser)
-            {
-                return true; 
-            }
-            return false; 
-        }
-
-        function logUserIn($username)
-        {
-            $_SESSION["login"] == true;
-        }
-        function logout()
-        {
-            return;
-        }
-        function redirectToLogin()
-        {
-            header("Location: ../views/login.view.php");
-        }
-    }
-
-    $obj = new UserAuthenticator;
-
-    if(!$obj -> isLoggedin())
-    {
-        $obj->redirectToLogin();
-
-        if(isset($_POST["enter"])) 
-        {
-            $username = $_POST["username"];
-            $password = $_POST["password"];
-
-            if($obj-> authenticate($username,$password))
-            {
-                $obj->logUserIn($username);
-                header("Location: ../views/index.view.php");
-
-            }
-        }
-    }
+require("UserAuthenticator.php");
+$user = new UserAuthenticator;
+$username = $_POST["username"];
+$password = $_POST["password"];
+if($user -> authenticate($username, $password)){
+    $user-> logUserIn();
+    header("Location: ../index.php");
+}else{
+    header("Location: ../login.php");
+}
     
 
 
