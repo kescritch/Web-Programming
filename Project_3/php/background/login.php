118d63c62e2b3a1f4a6ce8d21eeaff8b883f0b9a
Kyle Scritchfield <kescritch@gmail.com>
2024-12-02 12:49:42 -0600

    login

--- Project_3/php/background/login.php
+++ Project_3/php/background/login.php
@@ -1,30 +1,4 @@
-
 <?php
-/*
-    Background code for the login page 
-
-    Todo:
-     
-    Make a create account page
-        php
-    
-    make a class to verify email and password
-        get from database
-        redirect to the main page
-
-
-    inputs:
-        Email 
-        Password 
-        Enter button
-        Create account button - redirects to Create account page. 
-*/
-
-    if(isset($_POST['createAccount']))
-    {
-        header('Location: ..\html\createAccount.html');
-    }
-    
     session_start();
 
     class UserAuthenticator 
@@ -94,7 +68,4 @@
 
 
     
-
-
-
 
