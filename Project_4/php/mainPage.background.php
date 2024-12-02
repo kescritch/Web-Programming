ea08d7119eacaa00c67c2d55318f9d984e171f73
Kyle Scritchfield <kescritch@gmail.com>
2024-12-02 12:27:38 -0600

    html p4

---
+++ Project_4/php/mainPage.background.php
@@ -0,0 +1,22 @@
+<?php
+
+/* 
+    background for main page 
+
+    todo: 
+
+    redirect to login if not logged in 
+
+    make a popup to add a game to the list 
+        *add to flexbox
+        *add to database
+
+    find a way to remove a specific game
+        *remove flexbox
+        *remove from database
+
+    pull data from database and display into flexboxes
+
+    add game filtering
+*/
+
