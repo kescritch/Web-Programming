d60c1531c64cff5cc074161069c60919144a968f
Kyle Scritchfield <kescritch@gmail.com>
2024-12-02 21:48:33 -0600

    Got popups working

--- Project_4/js/mainPopups.js
+++ Project_4/js/mainPopups.js
@@ -1,8 +1,54 @@
-
-function add(){
-    window.open("../html/addGame.html", "Add Game", "width=500,height=300");
+function check(){
+  var val = document.getElementById("status").value
+  
+  if(val == "inProgress" || val == "notStarted")
+  {
+    document.getElementById("date").disabled = true
+  }
+  else
+  {
+    document.getElementById("date").disabled = false
+  }
 }
 
-function remove(){
-    window.open("https://www.example.com", "myWindow", "width=500,height=300");
+function open(popup)
+{
+    if(popup == null)
+    {
+        return
+    }
+
+    popup.classList.add('active')
+    overlay.classList.add('active')
+
 }
+
+function close(popup){
+    if(popup == null)
+        {
+            return
+        }
+    
+        popup.classList.remove('active')
+        overlay.classList.remove('active')
+    
+}
+
+
+const popupOpen = document.querySelectorAll('[data-popup-target]')
+const popupClose = document.querySelectorAll('[data-popup-close]')
+const overlay = document.getElementById('overlay')
+
+popupOpen.forEach(button => {
+    button.addEventListener("click", () =>{
+        const popup = document.querySelector(button.dataset.popupTarget)
+        open(popup)
+    })
+})
+
+popupClose.forEach(button => {
+    button.addEventListener("click", () =>{
+        const popup = button.closest('.popup')
+        close(popup)
+    })
+})
